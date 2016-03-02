d2c3a5a69e0788799e180d00029049459daf9719
Marc Dragunski <m.dragunski@uni-muenster.de>
2016-03-02 16:53:27 +0100

    changed imageBounds function

--- R/imageBounds.R
+++ R/imageBounds.R
@@ -9,10 +9,11 @@ imageBounds <- function(input){
   oSeM_df <- input
   
   coordinates(oSeM_df) =~longitude+latitude
-  proj4string(oSeM_df)="+proj=longlat +datum=WGS84"
-  project_df=spTransform(oSeM_df, CRS("+proj=longlat +datum=WGS84")) #projInfo(type="proj")
+#   proj4string(oSeM_df)="+proj=longlat +datum=WGS84"
+#   project_df=spTransform(oSeM_df, CRS("+proj=longlat +datum=WGS84")) #projInfo(type="proj")
   
   bbox <- bbox(oSeM_df)
-  list <- c(c(floor(bbox[2]), floor(bbox[1])),  c(ceiling(bbox[4]), ceiling(bbox[3])))
+#   list <- c(c(floor(bbox[2]), floor(bbox[1])),  c(ceiling(bbox[4]), ceiling(bbox[3])))
+  list <- c(c(bbox[2], bbox[1]),  c(bbox[4], bbox[3]))
   return(list)
 }
