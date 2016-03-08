24d9d2810de20d09add90f1d7f138e4e6d93ac1f
Marc Dragunski <m.dragunski@uni-muenster.de>
2016-03-08 12:59:17 +0100

    changed CRS in imageBounds to WebMercator :sparkles:

--- R/imageBounds.R
+++ R/imageBounds.R
@@ -9,10 +9,8 @@ imageBounds <- function(input){
   oSeM_df <- input
   
   coordinates(oSeM_df) =~longitude+latitude
-#   proj4string(oSeM_df)="+proj=longlat +datum=WGS84"
-#   project_df=spTransform(oSeM_df, CRS("+proj=longlat +datum=WGS84")) 
-#   proj4string(oSeM_df)="+init=epsg:3857"
-#   project_df=spTransform(oSeM_df, CRS("+init=epsg:3857")) #projInfo(type="proj")
+  proj4string(oSeM_df)="+init=epsg:3857"
+  project_df=spTransform(oSeM_df, CRS("+init=epsg:3857"))
   
   bbox <- bbox(oSeM_df)
   list <- c(c(floor(bbox[2]), floor(bbox[1])),  c(ceiling(bbox[4]), ceiling(bbox[3])))
